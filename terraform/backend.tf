ab485691ec265963bced745c0a1f003d486384d5
Imsk31 <shubhamkalekar8805@gmail.com>
2024-09-26 22:41:11 +0530

    changes in backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "31101999"
-    key    = "31101999/terraform_statefile/terraform.tfstate"
+    key    = "terraform_statefile/terraform.tfstate"
     region = "us-east-1"
   }
 }
