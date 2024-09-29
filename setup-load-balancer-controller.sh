718e0b6a3c669fdbbd81cc72b4fab259ad666eb1
Imsk31 <shubhamkalekar8805@gmail.com>
2024-09-29 21:40:25 +0530

    changes

--- setup-load-balancer-controller.sh
+++ setup-load-balancer-controller.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
@@ -13,9 +12,14 @@ eksctl utils associate-iam-oidc-provider --cluster $cluster_name --approve
 # Install AWS Load Balancer Controller with manifests
 curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.7.2/docs/install/iam_policy.json
 
-aws iam create-policy \
-    --policy-name AWSLoadBalancerControllerIAMPolicy \
-    --policy-document file://iam_policy.json
+# Check if the IAM policy already exists
+if aws iam get-policy --policy-arn arn:aws:iam::891377175977:policy/AWSLoadBalancerControllerIAMPolicy 2>/dev/null; then
+    echo "Policy AWSLoadBalancerControllerIAMPolicy already exists."
+else
+    aws iam create-policy \
+        --policy-name AWSLoadBalancerControllerIAMPolicy \
+        --policy-document file://iam_policy.json
+fi
 
 eksctl create iamserviceaccount \
   --cluster=$cluster_name \
@@ -30,17 +34,11 @@ kubectl apply \
     -f https://github.com/jetstack/cert-manager/releases/download/v1.13.5/cert-manager.yaml
 
 curl -Lo v2_7_2_full.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.7.2/v2_7_2_full.yaml
-
 sed -i.bak -e '612,620d' ./v2_7_2_full.yaml
-
-sed -i.bak -e 's|your-cluster-name|$cluster_name|' ./v2_7_2_full.yaml
-
+sed -i.bak -e "s|your-cluster-name|$cluster_name|" ./v2_7_2_full.yaml
 sed -i.bak -e 's|public.ecr.aws/eks/aws-load-balancer-controller|891377175977.dkr.ecr.us-east-1.amazonaws.com/eks/aws-load-balancer-controller|' ./v2_7_2_full.yaml
-
 kubectl apply -f v2_7_2_full.yaml
 
 curl -Lo v2_7_2_ingclass.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.7.2/v2_7_2_ingclass.yaml
-
 kubectl apply -f v2_7_2_ingclass.yaml
-
 kubectl get deployment -n kube-system aws-load-balancer-controller
