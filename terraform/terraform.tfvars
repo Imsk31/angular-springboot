65c5ebf0fef2eb6af90a98e78c7d18b748be5de7
Imsk31 <shubhamkalekar8805@gmail.com>
2024-10-01 00:03:53 +0530

    changes

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,11 +1,5 @@
 aws_region = "us-east-1"
-cluster_name = "msd"
-vpc_cidr = "10.123.0.0/16"
 azs = (["us-east-1a", "us-east-1b"])
-public_subnets = (["10.123.1.0/24", "10.123.2.0/24"])
-private_subnets = (["10.123.3.0/24", "10.123.4.0/24"])
-intra_subnets = (["10.123.5.0/24", "10.123.6.0/24"])
-vpc_name = "msd_vpc"
 db-name = "springbackend"
 rds_username = "admin"
 rds_password = "admin123"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,30 +2,6 @@ variable "aws_region" {
   type = string
   description = "AWS region for resources"
 }
-variable "cluster_name" {
-  type = string
-  description = "Cluster Name"
-}
-variable "vpc_name" {
-  type = string
-  description = "VPC Name"
-}
-variable "vpc_cidr" {
-  type = string
-  description = "VPC CIDR"
-}
-variable "public_subnets" {
-  type = list(string)
-  description = "Public Subnets"
-}
-variable "private_subnets" {
-  type = list(string)
-  description = "Private Subnets"
-}
-variable "intra_subnets" {
-  type = list(string)
-  description = "Intra Subnets for Cluter"
-}
 variable "azs" {
   type = list(string)
   description = "Availability Zones"
