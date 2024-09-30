ec12eec2edd24110d126d14816da158abd2f667d
Imsk31 <shubhamkalekar8805@gmail.com>
2024-09-30 18:45:52 +0530

    changes

--- setup-load-balancer-controller.sh
+++ setup-load-balancer-controller.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Creating OIDC Provider for cluster
-cluster_name=EKS-CLUSTER
+cluster_name=msd
 oidc_id=$(aws eks describe-cluster --name $cluster_name --query "cluster.identity.oidc.issuer" --output text | cut -d '/' -f 5)
 echo $oidc_id
 aws iam list-open-id-connect-providers | grep $oidc_id | cut -d "/" -f4
@@ -34,7 +34,7 @@ kubectl apply \
     -f https://github.com/jetstack/cert-manager/releases/download/v1.13.5/cert-manager.yaml
 curl -Lo v2_7_2_full.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.7.2/v2_7_2_full.yaml
 sed -i.bak -e '612,620d' ./v2_7_2_full.yaml
-sed -i.bak -e 's|your-cluster-name|EKS-CLUSTER|' ./v2_7_2_full.yaml
+sed -i.bak -e 's|your-cluster-name|msd|' ./v2_7_2_full.yaml
 kubectl apply -f v2_7_2_full.yaml
 curl -Lo v2_7_2_ingclass.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.7.2/v2_7_2_ingclass.yaml
 kubectl apply -f v2_7_2_ingclass.yaml
